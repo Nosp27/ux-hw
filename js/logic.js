aca4837ac4b740f60c7fb494b614ff4ce6f8ec29
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-12-14 01:22:39 +0300

    Done

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,28 @@
 // let state = '#intro';
 let age = "";
-let state = '#how-old';
+let month = "";
+let day = "";
+let state = '#intro';
+let startTime = 0;
+
+const monthNames = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec",
+];
 
 function transit(newState) {
     $(state).hide();
     state = newState;
-    $(state).show();
+    $(state).fadeIn();
 }
+
+$(function () {
+    startTime = Date.now();
+    fillAgeFrame();
+    fillMonthFrame();
+    fillDayFrame();
+});
 
 function fillAgeFrame () {
     const numbers = $("#numbers");
@@ -49,18 +65,40 @@ function fillAgeFrame () {
     eraseCloneWrapped.appendTo(numbers);
 }
 
+function fillMonthFrame(){
+    const container = $('#months-container');
+    for (let idx in monthNames) {
+        const m = monthNames[idx];
+        const clone = $('#m-num').clone();
+        clone.appendTo(container);
+        clone.show();
+        clone[0].id=`m-${m}`;
+        clone[0].innerHTML = m;
+        clone.click(function () {
+            selectMonth(m);
+        });
+    }
+}
+
 function fillDayFrame() {
     const monthLen = new Date(2000, new Date().getMonth() + 1, 0).getDate();
 
-    const numPrototype = $('#num');
-    const monthDays = $('#monthDays');
+    const numPrototype = $('#d-num');
+    const monthDays = $('#days-container');
 
     for (let i = 1; i <= monthLen; i++) {
         const clone = numPrototype.clone();
-        clone.show();
+        clone[0].hidden = false;
         clone[0].id = `month-day-${i}`;
-        clone[0].innerText = i.toString();
+        if (i < 10) {
+            clone[0].innerHTML = `<span style="color: transparent">0</span>${i}`
+        }
+        else
+            clone[0].innerText = i.toString();
         clone.appendTo(monthDays);
+        clone.click(function () {
+            selectDay(i);
+        })
     }
 }
 
@@ -70,21 +108,25 @@ function selectAgeDigit(digit) {
         if (age.length === 0)
             return;
         age = age.substr(0, age.length - 1);
+    } else if (digit === -100) { // skip
+        transit('#months');
     } else {
         age += digit.toString();
     }
 
     if (age.length === 2) {
-        transit('#month');
+        transit('#months');
     }
 
     let msg = "";
     switch (age.length) {
         case 0:
             msg = "How old are you?";
+            $('#done-age').hide();
             break;
         case 1:
             msg = `I am ${age}_`;
+            $('#done-age').show();
             break;
         case 2:
             msg = `I am ${age}`;
@@ -94,4 +136,25 @@ function selectAgeDigit(digit) {
             break;
     }
     $('#age')[0].innerText = msg;
+}
+
+function selectMonth(m) {
+    month = m;
+    transit('#days');
+}
+
+function selectDay(d) {
+    day = d;
+    prepareFinal();
+    transit('#final');
+}
+
+function prepareFinal() {
+    const a=parseInt(age);
+    const y = new Date();
+    y.setFullYear(new Date().getFullYear() - a);
+    const year = y.getFullYear();
+    $('#final-result').text(`${day} ${month} ${year}`);
+    $('.timing').show().text(`Your time is: ${(Date.now() - startTime) / 1000}s`);
+    $('#again').show();
 }
